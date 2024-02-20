7fc8924b8c7dc612415b7e26fd768bb70fc8b38b
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-02-19 22:07:22 -0500

    Created client file. Transferred & exported connect function.

--- play.js
+++ play.js
@@ -1,31 +1,6 @@
 const net = require("net");
+const connect = require('./client')
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',// IP address here,
-    port: 50541,// PORT number here,
-  });
-
-  //When connected to the client, log a message
-  conn.on('connect', () => {
-    console.log('Connected to server');
-  })
-
-  //When data is recieved, transform the data to a string
-  conn.on('data', (data) => {
-    console.log(data.toString());
-  })
-  // interpret incoming data as text
-  // conn.setEncoding("utf8");
-
-  //When the connection with the server is ended, log a disconnect message.
-  conn.on('end', () => {
-    console.log('Disconnected from server');
-  })
-  
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
