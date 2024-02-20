bc8e68858ef3a22083ed7ab595619140f37d2934
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-02-20 18:21:38 -0500

    Added comments

--- constants.js
+++ constants.js
@@ -1,10 +1,11 @@
 
-
+//Constants used in other files
 const IP = 'localhost'; // IP address 
 const PORT = 50541; //PORT number 
 
-const name = 'JEP'
+const name = 'JEP'//User initials
 
+//Contants being exported as properties inside of an object
 module.exports = {
   IP,
   PORT,
--- input.js
+++ input.js
@@ -2,6 +2,7 @@
 
 let connection;
 
+// Interface to handle user input from stdin
 const setupInput = (conn) => {
   connection = conn;
   const stdin = process.stdin;
@@ -12,16 +13,23 @@ const setupInput = (conn) => {
   return stdin;
 };
 
+//Function handles the user input
 const handleUserInput = function (data) {
+
+  //If the user keypress is 'ctrl + c' 
   if (data === '\u0003') {
-    console.log('Exiting Game')
+    console.log('Exiting Game') 
+    
+    //Delay by two seconds the closing of the game
     setTimeout(() => {
       process.exit();
     }, 2000)
     console.log('Disconnected from server')
+    //If the user presses the key 'm'
   } else if (data === 'm'){
     connection.write('Say: This game is amazing!')
   }
+    //Object with the corresponding actions per key press
     const movement = {
     'w': () => connection.write('Move: up'),
     's': () => connection.write('Move: down'),
@@ -29,8 +37,9 @@ const handleUserInput = function (data) {
     'd': () => connection.write('Move: right'),
   }
   let value = movement[data]
+  //if a certain key in this object is pressed
   if (value){
-    value();
+    value(); //Run the function corresponding to the key which triggers movement
   }
 };
 
